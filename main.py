731dd88a88af13e61269e23a154795c27203db15
ketandev22 <ketan.dev.py@gmail.com>
2026-09-05 23:58:59 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import random
+import json
+import string
+import os
+from flask import Flask,request,render_template,redirect,url_for
+
+app = Flask(__name__)
+
+shortened_urls = {}
+
+
+
+def generate_short_url(length=6):
+    char = string.ascii_letters + string.digits
+    short_url = "".join(random.choice(char)for _ in range(length))
+    return short_url
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method=='POST':
+        long_url = request.form["long_url"]
+        if not long_url.startswith(("https://","http://")):
+            long_url = "https://"+long_url
+        short_url = generate_short_url()
+        while short_url in shortened_urls:
+            short_url=generate_short_url()
+
+        shortened_urls[short_url]=long_url
+        with open("urls.json","w") as f:
+            json.dump(shortened_urls,f)
+        return f"Shortend URL -  {request.url_root}{short_url}"
+
+    return render_template("index.html")
+
+@app.route("/<short_url>")
+def redirect_url(short_url):
+    long_url = shortened_urls.get(short_url)
+    if long_url:
+        return redirect (long_url)
+    else:
+        return "NO URL FOUND",404
+    
+
+if __name__=="__main__":
+    if os.path.exists("urls.json"):
+        with open ("urls.json","r") as f:
+            try:
+                shortened_urls = json.load(f)
+            except json.JSONDecodeError:
+                shortened_urls={}
+                
+    app.run(debug=True,port=8000)
